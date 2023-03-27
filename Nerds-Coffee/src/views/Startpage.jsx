d02e82a72a908d10a98fd7b08146d0e7937a7139
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-03-27 14:32:41 +0200

    Startpage done

--- Nerds-Coffee/src/views/Startpage.jsx
+++ Nerds-Coffee/src/views/Startpage.jsx
@@ -1,8 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import './Startpage.css';
+
 function Startpage() {
-
+  const navigation = useNavigate();
+  
+  function handleClick() {
+    navigation("/menu")
+  }
+  
     return (
-      <section>
-
+      <section className='startpage' onClick={handleClick}>
+        <article>
+          <img src="/src/views/img/intro-graphic-left.svg" alt="graphicLeft" />
+        </article> 
+        <article className="logo">
+          <img src="/src/views/img/airbean-landing.svg" alt="graphicRight" />
+        </article>
+        <article>
+          <img src="/src/views/img/intro-graphic-right.svg" alt="logo" />
+        </article>
       </section>
     )
   }
