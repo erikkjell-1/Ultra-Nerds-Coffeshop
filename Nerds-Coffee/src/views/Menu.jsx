d7ccf42999c5470adf9b47f25dc6418244bbf791
erikkjell-1 <erkan26@live.se>
2023-03-28 11:46:17 +0200

    cartNumber fixed

--- Nerds-Coffee/src/views/Menu.jsx
+++ Nerds-Coffee/src/views/Menu.jsx
@@ -3,9 +3,10 @@ import CoffeeCard from "../components/CoffeeCard"
 import { useState, useEffect } from "react"
 import Slider from "../components/Slider";
 import './Menu.css'
+import { useSelector } from "react-redux";
 
 function Menu() {
-
+  const orders = useSelector((state) => { return state.order })
   const [menu, setMenu] = useState([]);
   const [isShown, setIsShown] = useState(false)
 
@@ -47,9 +48,9 @@ function Menu() {
 </svg>
 <button className='cart'onClick={ handleClick }>
 <svg width="16" height="22" viewBox="0 0 16 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd" clipRule="evenodd" d="M10.0486 4.04876V6.77232H5.95103V4.04876C5.95103 2.91726 6.86829 2 7.99979 2C9.13129 2 10.0486 2.91726 10.0486 4.04876ZM3.95103 4.04876C3.95103 1.81269 5.76373 0 7.99979 0C10.2359 0 12.0486 1.81269 12.0486 4.04876V6.77232V7.42274H13.5427C14.6103 7.42274 15.4896 8.26123 15.5404 9.32761L15.9977 18.9314C16.0521 20.0721 15.1419 21.0266 14 21.0266H1.99997C0.858047 21.0266 -0.0520865 20.0721 0.00222909 18.9314L0.459555 9.32761C0.510335 8.26123 1.38971 7.42274 2.45729 7.42274H3.95103V6.77232V4.04876ZM2.45729 9.42274H13.5427L14 19.0266H1.99997L2.45729 9.42274Z" fill="white"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M10.0486 4.04876V6.77232H5.95103V4.04876C5.95103 2.91726 6.86829 2 7.99979 2C9.13129 2 10.0486 2.91726 10.0486 4.04876ZM3.95103 4.04876C3.95103 1.81269 5.76373 0 7.99979 0C10.2359 0 12.0486 1.81269 12.0486 4.04876V6.77232V7.42274H13.5427C14.6103 7.42274 15.4896 8.26123 15.5404 9.32761L15.9977 18.9314C16.0521 20.0721 15.1419 21.0266 14 21.0266H1.99997C0.858047 21.0266 -0.0520865 20.0721 0.00222909 18.9314L0.459555 9.32761C0.510335 8.26123 1.38971 7.42274 2.45729 7.42274H3.95103V6.77232V4.04876ZM2.45729 9.42274H13.5427L14 19.0266H1.99997L2.45729 9.42274Z" fill="white"/>
 </svg>
-
+<aside className="cart__number">{ orders.length }</aside>
 </button>
       
       {isShown && (
